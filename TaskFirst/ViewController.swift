acc8baca4065c027abc9ac5080388a02b1c89fa8
xvsvn <86996400+xvsvn@users.noreply.github.com>
2023-05-21 11:25:46 +0500

    Initial Commit

---
+++ TaskFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskFirst
+//
+//  Created by Xasan Xasanov on 21/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
